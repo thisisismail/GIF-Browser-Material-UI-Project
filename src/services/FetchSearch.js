522b1eda6e39658381962a638a034619e0b9f6bd
thisisismail <simail.official@gmail.com>
2022-05-16 16:01:10 +0700

    API KEY to Env

--- src/services/FetchSearch.js
+++ src/services/FetchSearch.js
@@ -1,13 +1,13 @@
 const FetchSearch = (v) => {
   let SEARCH = v;
-  const APIKEY = 'q9OHrAuU0weVyvMHedp59fzZSH0CxEbI';
+  const APIKEY = process.env.REACT_APP_API_KEY;
   const URL = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&q=${SEARCH}&limit=12`;
   console.log('Start loading....');
   return fetch(URL);
 };
 
 const FetchSearchTrending = () => {
-  const APIKEY = 'q9OHrAuU0weVyvMHedp59fzZSH0CxEbI';
+  const APIKEY = process.env.REACT_APP_API_KEY;
   const URL = `https://api.giphy.com/v1/gifs/trending?api_key=${APIKEY}&limit=12`;
   console.log('Start loading....');
   return fetch(URL);
