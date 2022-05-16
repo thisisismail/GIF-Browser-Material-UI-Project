b659c9e16c27f70d611b92d2b84e86d4bf06ee5d
thisisismail <simail.official@gmail.com>
2022-05-16 16:40:04 +0700

    Homepage Background

--- src/parts/content/home/index.js
+++ src/parts/content/home/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import {connect} from 'react-redux';
+let source = 'https://images.unsplash.com/photo-1530347927633-5f393ff7abd0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80';
+    
 
 function Home() {
-
+  const myStyle={
+    height: '100vh',
+    width: '100%',
+    objectFit: 'cover',
+  };
   return (
     <div>
+      <img src={source} style={myStyle} alt="Background"/>
     </div>
   )
 }
